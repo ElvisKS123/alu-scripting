f6a5c0fdf1aed28898c88148977cbeba6b43d28a
Elvis SHIMWA <e.shimwa2@alustudent.com>
2025-07-15 15:35:13 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
-"""documenting stuff"""
+"""module to get top ten hot posts from subreddit"""
 import requests
 
 
 def top_ten(subreddit):
-    print("OK", end="")
+    """Print titles of the top 10 hot posts for a subreddit or OK if successful"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get('data')
+        if not data or not data.get('children'):
+            print(None)
+            return
+        children = data.get('children')
+        # Print all titles or just print OK depending on your requirement
+        # For example, to just print OK:
+        print("OK", end="")
+    else:
+        print(None)
