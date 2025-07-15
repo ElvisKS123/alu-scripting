b026ec60857f268f36adde60c5b8141e8fb98a57
Elvis SHIMWA <e.shimwa2@alustudent.com>
2025-07-15 15:14:51 +0200

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,7 +9,6 @@ def top_ten(subreddit):
         .format(subreddit)
     headers = headers = {'User-agent': 'Mozilla/5.0'}
     response = requests.get(reddit_url, headers=headers)
-
     if response.status_code == 200:
         data = response.json()['data']
         for post in data['children'][:10]:
