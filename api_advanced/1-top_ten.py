7ab3b2fbf244566933661124740c1f5f1188ab2a
Elvis SHIMWA <e.shimwa2@alustudent.com>
2025-07-15 15:09:56 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,14 +5,18 @@ import requests
 
 def top_ten(subreddit):
     """Docs"""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    headers = headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url, headers=headers)
+    reddit_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()['data']
+        data = response.json().get('data')
+        if not data or 'children' not in data:
+            print(None)
+            return
+
         for post in data['children'][:10]:
             print(post['data']['title'])
     else:
         print(None)
+        
